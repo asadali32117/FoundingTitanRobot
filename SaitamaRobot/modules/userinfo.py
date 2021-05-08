2208b5ee81a4b3d09a74ce1c732296191774da72
PAINBOI2008 <81083883+PAINBOI2008@users.noreply.github.com>
2021-05-08 18:34:42 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/foundingtitanupdates/12">?</a>]'.format(
             bot.username,
         )
 
