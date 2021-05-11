4fce289f17756f7fd6bc8c517a36b5948849d188
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-12 02:18:51 +0530

    Update __main__.py
    Nexus Network

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -73,12 +73,12 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 Hey hi {}, I'm {}!
 I am an Anime themed group management bot.
-Built by weebs for weebs, I specialize in managing anime eccentric communities,Do /help to see all commands 
+Built by weebs for weebs, I specialize in managing anime eccentric communities,Do /help to see list of all the commands. 
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Titan-Shifting Group Management Bot and help admins manage their groups with My Titan Powers! Have a look at the following for an idea of some of \
+I'm The Founding Titan Group Management Bot and help admins manage their groups with My Titan Powers! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
@@ -224,7 +224,7 @@ def start(update: Update, context: CallbackContext):
                         ],
                         [
                             InlineKeyboardButton(
-                                text="🚑 Support",
+                                text="☎️ Support",
                                 url=f"https://t.me/{SUPPORT_CHAT}",
                             ),
                             InlineKeyboardButton(
@@ -234,18 +234,18 @@ def start(update: Update, context: CallbackContext):
                         ],
                         [
                             InlineKeyboardButton(
-                                text="🧾 Getting Started",
+                                text="📋 Getting Started",
                                 url="https://t.me/foundingtitanupdates/4",
                             ),
                             InlineKeyboardButton(
-                                text="🗄 Source code",
+                                text="📂 Source code",
                                 url="https://github.com/sasukeuchiha912/FoundingTitanRobot",
                             ),
                         ],
                         [
                             InlineKeyboardButton(
-                                text="☠️ Anime Chat Group",
-                                url="https://t.me/otaku_regiment/",
+                                text="⚡ Nexus Network",
+                                url="https://t.me/nexusgroups/",
                             ),
                         ],
                     ],
