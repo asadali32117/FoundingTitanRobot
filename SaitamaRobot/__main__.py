04260ccbd5e5ba83249c0b2fe43f582d9288bae2
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-16 02:55:16 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -79,15 +79,7 @@ Built by weebs for weebs, I specialize in managing anime eccentric communities.
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm The Founding Titan Group Management Bot and help admins manage their groups with My Titan Powers! Have a look at the following for an idea of some of \
-the things I can help you with.
-
-*Main* commands available:
- • /help: PM's you this message.
- • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
- • /settings:
-   • in PM: will send you your settings for all supported modules.
-   • in a group: will redirect you to pm, with all that chat's settings.
+the things I can help you with.Reach out at @FoundingTitansupport for any help [.] (https://telegra.ph/file/d92be12b160769ddd5e5b.jpg)
 
 
 {}
