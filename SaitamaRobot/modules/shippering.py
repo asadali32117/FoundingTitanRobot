425718adccfb2e19929ea1a9a24985d37cd67cbf
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-17 00:41:35 +0530

    Update shippering.py

--- SaitamaRobot/modules/shippering.py
+++ SaitamaRobot/modules/shippering.py
@@ -77,7 +77,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __help__ = """
-/couples - To Choose Couple Of The Day
+/couples = To Choose Couple Of The Day
 
 
 __mod_name__ = "Couples"
