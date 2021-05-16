96cec5b571918547cef459afb77e97aed016e183
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-17 02:08:54 +0530

    Update shippering.py

--- SaitamaRobot/modules/shippering.py
+++ SaitamaRobot/modules/shippering.py
@@ -71,9 +71,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
                 message.chat.id,
                 text=couple_selection_message
             )
-      except Exception as e:
-          print(e)
-          await message.reply_text(e)
+      
 
 
 __help__ = """
