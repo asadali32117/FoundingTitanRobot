84cea7dbd8827748891e5716962ddcb2428e5b7a
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-23 02:47:20 +0530

    Update searchall.py

--- SaitamaRobot/modules/searchall.py
+++ SaitamaRobot/modules/searchall.py
@@ -165,12 +165,12 @@ async def apk(e):
 __mod_name__ = "◎Search"
 
 __help__ = """
- • /google <text>*:* Perform a google search
- • /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
- • /app <appname>*:* Searches for an app in Play Store and returns its details.
- • /reverse: Does a reverse image search of the media which it was replied to.
- • /gps <location>*:* Get gps location.
- • /github <username>*:* Get information about a GitHub user.
- • /country <country name>*:* Gathering info about given country
- • /imdb <Movie name>*:* Get full info about a movie with imdb.com
+ • #/google <text>*:* Perform a google search
+ • #/img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ • #/app <appname>*:* Searches for an app in Play Store and returns its details.
+ • #/reverse: Does a reverse image search of the media which it was replied to.
+ • #/gps <location>*:* Get gps location.
+ • #/github <username>*:* Get information about a GitHub user.
+ • #/country <country name>*:* Gathering info about given country
+ • #/imdb <Movie name>*:* Get full info about a movie with imdb.com
  
