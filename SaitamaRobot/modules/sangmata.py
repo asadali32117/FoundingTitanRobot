285bd5547932ba034e2767dd2fc178d2dd593e65
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-23 04:08:34 +0530

    Update sangmata.py

--- SaitamaRobot/modules/sangmata.py
+++ SaitamaRobot/modules/sangmata.py
@@ -4,7 +4,6 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from SaitamaRobot.util import admin_cmd
 from SaitamaRobot.events import register
 
 @register(pattern="^/sg (.*)")
