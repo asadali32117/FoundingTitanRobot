d675790ebb74543cab528cf0ff59e9f76ff6658e
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-24 01:17:13 +0530

    Update imdb.py

--- SaitamaRobot/modules/imdb.py
+++ SaitamaRobot/modules/imdb.py
@@ -108,3 +108,9 @@ async def imdb(e):
         )
     except IndexError:
         await e.reply("Need Valid movie/show/anime name")
+
+__help__ = """
+  • `/imdb` <show/movie/anime> :- get ratings and info of any show/movie/anime from [IMDB](https://m.imdb.com/)
+"""
+__mod_name__ = "IMDB"
+
