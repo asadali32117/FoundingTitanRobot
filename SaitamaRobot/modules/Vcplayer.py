3b5f1beb47644c39ff4690882c68b5aa8520c045
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-26 02:28:43 +0530

    Create Vcplayer.py

---
+++ SaitamaRobot/modules/Vcplayer.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2021 TeamDaisyX
+
+
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__mod_name__ = "VC PLAYER"
+__help__ = """
+          
+            
+ Eren Jaeger also plays music in your group's voice chat
+ 
+ Assistant username: @FoundingTitanVCRobot
+ 
+<b> Setting up </b>
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+4) If userbot joined enjoy music, If not add @FoundingTitanVCRobot manually to your group and retry
+ *Commands*
+=>> Song Playing 🎶
+  /play: Play song using youtube music
+  /play [yt url] : Play the given yt url
+  /dplay: Play song via deezer
+  /splay: Play song via jio saavn
+  
+=>> Playback ⏯
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist
+=>> More tools 📲
+  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @FoundingTitanVCRobot Userbot to your chat
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME
+"""
