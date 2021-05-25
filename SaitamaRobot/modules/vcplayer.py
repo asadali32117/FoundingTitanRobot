9207a3d4da60d26e1bf3b9598b59bec98b3709d5
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-26 02:57:30 +0530

    Update vcplayer.py

--- SaitamaRobot/modules/vcplayer.py
+++ SaitamaRobot/modules/vcplayer.py
@@ -18,25 +18,26 @@
 
 __mod_name__ = "VC PLAYER"
 __help__ = """
-          》** EREN VC PLAYER ** 《
+          》EREN VC PLAYER《
             
 Eren Jaeger can also play music in your group's voice chat
  
-Assistant username >> @FoundingTitanVCRobot
+Assistant username:- @FoundingTitanVCRobot
  
-<b> Setting up </b>
+ Setting up 
 1) Make bot admin
 2) Start a voice chat
 3) Try `/play [song name]` for the first time by an admin
-*) If userbot joined enjoy music, If not add @FoundingTitanVCRobot to your group manually and retry
-<b> Commands </b>
-=>> Song Playing 🎶
+) If userbot joined enjoy music, If not add @FoundingTitanVCRobot to your group manually and retry
+ Commands:
+ 
+• Song Playing 🎶
   `/play`: Play song using youtube music
   `/play` <yt url> : Play the given yt url
   `/dplay`: Play song via deezer
   `/splay`: Play song via jio saavn
   
-=>> Playback ⏯
+• Playback ⏯
   `/player`: Open Settings menu of player
   `/skip`: Skips the current track
   `/pause`: Pause track
@@ -44,9 +45,9 @@ Assistant username >> @FoundingTitanVCRobot
   `/end`: Stops media playback
   `/current`: Shows the current Playing track
   `/playlist`: Shows playlist
-=>> More tools 📲
+• More tools 📲
   `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
   `/userbotjoin`: Invite @FoundingTitanVCRobot to your chat
-*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
-<b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group
+ PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME 
 """
