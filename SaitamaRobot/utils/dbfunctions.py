046b7d47ef8d9fb38b477e86e40dd50ed1d3bcaa
Sasuke Uchiha <80569174+sasukeuchiha912@users.noreply.github.com>
2021-05-26 23:59:33 +0530

    Update dbfunctions.py

--- SaitamaRobot/utils/dbfunctions.py
+++ SaitamaRobot/utils/dbfunctions.py
@@ -14,32 +14,22 @@ async def get_karmas_count() -> dict:
     chats_count = 0
     karmas_count = 0
     for chat in await chats.to_list(length=1000000):
-        for i in chat["karma"]:
-            karma_ = chat["karma"][i]["karma"]
-            if karma_ > 0:
-                karmas_count += karma_
+        for i in chat['karma']:
+            karmas_count += chat['karma'][i]['karma']
         chats_count += 1
-    return {"chats_count": chats_count, "karmas_count": karmas_count}
-
-
-async def user_global_karma(user_id) -> int:
-    chats = karmadb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return 0
-    total_karma = 0
-    for chat in await chats.to_list(length=1000000):
-        karma = await get_karma(chat["chat_id"], await int_to_alpha(user_id))
-        if karma:
-            if int(karma["karma"]) > 0:
-                total_karma += int(karma["karma"])
-    return total_karma
+    return {
+        "chats_count": chats_count,
+        "karmas_count": karmas_count
+    }
 
 
 async def get_karmas(chat_id: int) -> Dict[str, int]:
     karma = await karmadb.find_one({"chat_id": chat_id})
-    if not karma:
-        return {}
-    return karma["karma"]
+    if karma:
+        karma = karma['karma']
+    else:
+        karma = {}
+    return karma
 
 
 async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
@@ -54,30 +44,14 @@ async def update_karma(chat_id: int, name: str, karma: dict):
     karmas = await get_karmas(chat_id)
     karmas[name] = karma
     await karmadb.update_one(
-        {"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True
+        {"chat_id": chat_id},
+        {
+            "$set": {
+                "karma": karmas
+            }
+        },
+        upsert=True
     )
-
-
-async def is_karma_on(chat_id: int) -> bool:
-    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
-    if not chat:
-        return True
-    return False
-
-
-async def karma_on(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if is_karma:
-        return
-    return await karmadb.delete_one({"chat_id_toggle": chat_id})
-
-
-async def karma_off(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if not is_karma:
-        return
-    return await karmadb.insert_one({"chat_id_toggle": chat_id})
-
 async def int_to_alpha(user_id: int) -> str:
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
     text = ""
